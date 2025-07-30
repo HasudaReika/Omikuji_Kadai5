1b746f6f8d94cddfd3934ad4ce6f27d5e93022e5
r_hasuda <r_hasuda@192.168.40.106>
2025-07-30 14:59:23 +0900

    入力された誕生日の過去半年の結果をリストで表示する画面を作成

--- omikuji_kadai5/src/main/java/omikuji5/ListAction.java
+++ omikuji_kadai5/src/main/java/omikuji5/ListAction.java
@@ -1,7 +1,11 @@
 package omikuji5;
 
 import java.sql.SQLException;
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,25 +19,37 @@ import org.apache.struts.action.ActionMapping;
 /*
  * 入力された誕生日の過去半年の結果をリスト化
  */
-public class ListAction extends Action{
+public class ListAction extends Action {
 
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response) throws ClassNotFoundException, SQLException {
 		OmikujiDB omikujiDB = new OmikujiDB();
 		List<OmikujiResult> results = null;
 
+		//アクションフォーム
+		OmikujiForm birthdayActionForm = (OmikujiForm) form;
 		//誕生日文字列を受け取る
-		String birthday = request.getParameter("birthday");
+		String birthday = birthdayActionForm.getBirthday();
 
-		//String型からLocalDate型に変換
-		LocalDate bdDate = LocalDate.parse(birthday);
+		//birthdayをLocalDate型に変換
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+		//変換後の今日の日付
+		LocalDate bdDate = null;
+
+		//誕生日文字列を半角に変換
+		String birthdayString = Normalizer.normalize(birthday, Form.NFKC);
+		try {
+			bdDate = LocalDate.parse(birthdayString, formatter);
+		} catch (DateTimeParseException e) {
+			e.printStackTrace();
+		}
+
 		//過去半年の結果を取得		
 		results = omikujiDB.getResultPastSixMonths(bdDate);
 		//リクエストスコープにリストをセット
 		request.setAttribute("results", results);
 		//リクエストスコープに誕生日をセット
-		request.setAttribute("birthday", birthday);
-		
+		request.setAttribute("birthday", bdDate);
 
 		return mapping.findForward("success");
 
--- omikuji_kadai5/src/main/webapp/listPerBirthday.jsp
+++ omikuji_kadai5/src/main/webapp/listPerBirthday.jsp
@@ -3,15 +3,17 @@
     pageEncoding="UTF-8"%>
 <%@taglib uri="http://struts.apache.org/tags-html" prefix="html" %>
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 <%@page isELIgnored="false" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>誕生日ごとのリスト表示</title>
+<link rel="stylesheet" href="${pageContext.request.contextPath}/css/style2.css">
 </head>
 <body>
-<h1>${omikuji.getBirthday()}の過去半年の占い結果</h1>
+<h1>${birthday}の過去半年の占い結果</h1>
 
 <table border="1">
 <tr>
@@ -21,17 +23,17 @@
 	<th>商い</th>
 	<th>学問</th>
 </tr>
-<c:forEach var="v" results="${results}">
+<c:forEach var="v" items="${results}">
 <tr>
-	<td>${omikujiResult.fortuneTellingDate}</td>
-	<td>${omikujiResult.fortuneName}</td>
-	<td>${omikujiResult.negaigoto}</td>
-	<td>${omikujiResult.akinai}</td>
-	<td>${omikujiResult.gakumon}</td>
+	<td>${v.fortuneTellingDate}</td>
+	<td>${v.fortuneName}</td>
+	<td>${v.negaigoto}</td>
+	<td>${v.akinai}</td>
+	<td>${v.gakumon}</td>
 </tr>
 </c:forEach>
 </table>
-
+<a href="javascript:history.back()">前のページへ</a>
 <a href="http://localhost:8080/omikuji_kadai5/birthdayInput.jsp">誕生日入力画面に戻る</a>
 </body>
 </html>
